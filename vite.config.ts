7fe981b80bba816361e47233550d3f5ea2b12c50
Shivesh4 <shiveshsar@gmail.com>
2025-08-04 23:47:45 -0500

    Hosting in gh-pages 2nd version

--- vite.config.ts
+++ vite.config.ts
@@ -26,9 +26,16 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-export default defineConfig({ 
+export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio",
+  base: "/Portfolio/",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
+
 
