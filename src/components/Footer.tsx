8b9bfa7dd2f56a7087fbff1d4d34418ec3fa246a
Shivesh4 <shiveshsar@gmail.com>
2025-08-07 23:57:00 -0500

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,10 @@
-import { Heart, Code } from 'lucide-react';
 
 const Footer = () => {
   return (
     <footer className="py-8 border-t border-card-border bg-surface">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          {/* Copyright */}
-          {/* {/* <div className="flex items-center text-text-muted mb-4 md:mb-0"> */}
-            {/* <span>© 2024 Shivesh Saravanan. Made with</span>
-            <Heart className="w-4 h-4 mx-2 text-primary animate-pulse" />
-            <span>and</span>
-            <Code className="w-4 h-4 ml-2 text-secondary" />
-          </div> */}
-
-          {/* Quick Links */}
+          
           <div className="flex justify-center items-center space-x-6 text-text-muted">
             <a href="#home" className="hover:text-primary transition-colors">Home</a>
             <a href="#about" className="hover:text-primary transition-colors">About</a>
